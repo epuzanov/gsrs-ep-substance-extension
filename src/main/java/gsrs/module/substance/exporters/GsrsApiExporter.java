3023e245ed6c29dd919dceaaee9f6f958f147929
epuzanov <epuzanov@users.noreply.github.com>
2022-10-21 17:29:16 +0200

    switch to apache HTTPClient

--- src/main/java/gsrs/module/substance/exporters/GsrsApiExporter.java
+++ src/main/java/gsrs/module/substance/exporters/GsrsApiExporter.java
@@ -2,6 +2,8 @@ package gsrs.module.substance.exporters;
 
 import com.fasterxml.jackson.databind.ObjectWriter;
 import ix.core.controllers.EntityFactory;
+import ix.core.validator.GinasProcessingMessage;
+import ix.core.validator.ValidationResponse;
 import ix.ginas.exporters.Exporter;
 import ix.ginas.models.v1.Substance;
 
@@ -9,27 +11,51 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.util.Map;
 import java.util.Objects;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by Egor Puzanov on 10/18/22.
  */
 public class GsrsApiExporter implements Exporter<Substance> {
+
+    private final HttpHeaders headers;
     private final BufferedWriter out;
     private final RestTemplate restTemplate;
-    private final ObjectWriter writer =  EntityFactory.EntityMapper.FULL_ENTITY_MAPPER().writer();
+    private final ObjectWriter writer = EntityFactory.EntityMapper.FULL_ENTITY_MAPPER().writer();
 
-    public GsrsApiExporter(OutputStream out, RestTemplate restTemplate) throws IOException{
+    public GsrsApiExporter(OutputStream out, RestTemplate restTemplate, Map<String, String> headers) throws IOException {
         Objects.requireNonNull(out);
         this.out = new BufferedWriter(new OutputStreamWriter(out));
         Objects.requireNonNull(restTemplate);
         this.restTemplate = restTemplate;
+        HttpHeaders h = new HttpHeaders();
+        h.setContentType(MediaType.APPLICATION_JSON);
+        for (Map.Entry<String, String> entry : headers.entrySet()) {
+            h.set(entry.getKey(), entry.getValue());
+        }
+        this.headers = h;
     }
 
     @Override
     public void export(Substance obj) throws IOException {
-        out.write(writer.writeValueAsString(obj));
+        try {
+            obj.version = restTemplate.getForObject("/{uuid}/version", String.class, obj.getUuid().toString());
+        } catch (Exception ex) {
+            obj.version = "1";
+        }
+        HttpEntity<String> request = new HttpEntity<String>(writer.writeValueAsString(obj), headers);
+        ValidationResponse vr = restTemplate.postForObject("/@validate", request, ValidationResponse.class);
+        try {
+            restTemplate.put("/", request);
+            out.write(obj.getUuid().toString() + " " + obj.getName() + " - SUCCESS");
+        } catch (Exception ex) {
+            out.write(obj.getUuid().toString() + " " + obj.getName() + " - ERROR: " + ex.getMessage());
+        }
         out.newLine();
     }
 
--- src/main/java/gsrs/module/substance/exporters/GsrsApiExporterFactory.java
+++ src/main/java/gsrs/module/substance/exporters/GsrsApiExporterFactory.java
@@ -11,19 +11,26 @@ import ix.ginas.models.v1.Substance;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.security.SecureRandom;
-import java.security.cert.X509Certificate;
-import java.time.Duration;
+import java.security.NoSuchAlgorithmException;
 import java.util.Collections;
+import java.util.Map;
 import java.util.Set;
-import javax.net.ProxySelector;
-import javax.net.http.HttpClient;
+import java.util.stream.Collectors;
 import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLParameters;
 
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustAllStrategy;
+import org.apache.http.conn.ssl.AllowAllHostnameVerifier;
+import org.apache.http.conn.ssl.SSLContexts;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+
 import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.DefaultUriBuilderFactory;
 
 
 /**
@@ -32,22 +39,10 @@ import org.springframework.web.client.RestTemplate;
 public class GsrsApiExporterFactory implements ExporterFactory {
 
     private OutputFormat format = new OutputFormat("gsrsapi", "Send to the other GSRS instance");
-    private int timeout = 50;
+    private int timeout = 120000;
     private boolean trustAllCerts = false;
-    private Map<String, String> headers = new HashMap<String, String>();
-    private final TrustManager[] trustAllCerts = new TrustManager[]{
-        new X509TrustManager() {
-            public X509Certificate[] getAcceptedIssuers() {
-                return null;
-            }
-            public void checkClientTrusted(
-               X509Certificate[] certs, String authType) {
-            }
-            public void checkServerTrusted(
-               X509Certificate[] certs, String authType) {
-           }
-        }
-    };
+    private String baseUrl = "http://localhost:8080/api/v1/substances";
+    private Map<String, String> headers;
 
     public void setFormat(Map<String, String> m) {
         this.format = new OutputFormat(m.get("extension"), m.get("displayName"));
@@ -59,6 +54,10 @@ public class GsrsApiExporterFactory implements ExporterFactory {
 
     public void setTrustAllCerts(boolean trustAllCerts) {
         this.trustAllCerts = trustAllCerts;
+    }
+
+    public void setBaseUrl(String baseUrl) {
+        this.baseUrl = baseUrl;
     }
 
     public void setHeaders(Map<String, String> headers) {
@@ -77,29 +76,27 @@ public class GsrsApiExporterFactory implements ExporterFactory {
 
     @Override
     public Exporter<Substance> createNewExporter(OutputStream out, Parameters params) throws IOException {
-        SSLContext sslContext = SSLContext.getDefault();
-        SSLParameters sslParameters = sslContext.getDefaultSSLParameters();
-        if (trustAllCerts) {
-            sslContext.init(null, trustAllCerts, new SecureRandom());
-            sslParameters.setEndpointIdentificationAlgorithm(null);
-        }
-        HttpClient client = HttpClient.newBuilder()
-            .connectTimeout(Duration.ofSeconds(timeout))
-            .proxy(ProxySelector.getDefault())
-            .sslContext(sslContext)
-            .sslParameters(sslParameters)
+        RequestConfig requestConfig = RequestConfig.custom()
+            .setConnectionRequestTimeout(timeout)
             .build();
-        ClientHttpRequestFactory clientFactory = new HttpComponentsClientHttpRequestFactory(client);
-        RestTemplate restTemplate = new RestTemplate(clientFactory).getInterceptors().add(new ClientHttpRequestInterceptor(){
-            @Override
-            public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
-                for (Map.Entry<String, String> entry, headers.entrySet()) {
-                    request.getHeaders().set(entry.getKey(), entry.getValue());
-                }
-                return execution.execute(request, body);
+        HttpClientBuilder client = HttpClients.custom()
+            .useSystemProperties()
+            .setDefaultRequestConfig(requestConfig);
+        if (trustAllCerts) {
+            try {
+                SSLContext sslContext = SSLContexts.custom()
+                    .loadTrustMaterial(null, new TrustAllStrategy())
+                    .useTLS()
+                    .build();
+                SSLConnectionSocketFactory connectionFactory = new SSLConnectionSocketFactory(sslContext, new AllowAllHostnameVerifier());
+                client = client.setSSLSocketFactory(connectionFactory);
+            } catch (Exception ex) {
             }
-        });
-        return new GsrsApiExporter(out, restTemplate);
+        }
+        ClientHttpRequestFactory clientFactory = new HttpComponentsClientHttpRequestFactory(client.build());
+        RestTemplate restTemplate = new RestTemplate(clientFactory);
+        restTemplate.setUriTemplateHandler(new DefaultUriBuilderFactory(baseUrl));
+        return new GsrsApiExporter(out, restTemplate, headers);
     }
 
     //@Override
